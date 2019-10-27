60ee61a32e4a3c2e463c59eadc3023734e8eb083
marde12345 <32907132+marde12345@users.noreply.github.com>
2019-10-27 23:58:18 +0700

    Add progress bar, error, and success

--- android-client/app/src/main/java/com/example/indoorpositioning/FetchData.java
+++ android-client/app/src/main/java/com/example/indoorpositioning/FetchData.java
@@ -1,9 +1,11 @@
 package com.example.indoorpositioning;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.indoorpositioning.Config.Config;
@@ -20,35 +22,33 @@ import org.json.JSONException;
 
 import java.io.IOException;
 
+import androidx.annotation.Nullable;
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
-public class FetchData extends AsyncTask<String, Integer, String> {
+
+public class FetchData extends AsyncTask<String, Integer, Integer> {
     private String baseUrl = Config.BASE_URL;
     private Context context;
+    private SweetAlertDialog dialogProgress, dialogWarning, dialogError, dialogInfo;
 
-    public FetchData(Context context){
-        this.context=context;
+    public FetchData(Context context) {
+        this.context = context;
     }
+
     @Override
-    protected String doInBackground(String... params) {
-
-        return postData();
+    protected void onPreExecute() {
+        super.onPreExecute();
+        showProgress("Proses", "Harap Tunggu");
     }
 
-    protected void onPostExecute(String status) {
-
-            Toast.makeText(context,status, Toast.LENGTH_LONG).show();
-    }
-
-
-    protected void onProgressUpdate(Integer... progress) {
-
-    }
-
-    public String postData() {
+    @Override
+    protected Integer doInBackground(String... params) {
         // Create a new HttpClient and Post Header
         HttpClient httpclient = new DefaultHttpClient();
         HttpGet httpGet = new HttpGet(baseUrl + "");
         String json = null;
+
+        publishProgress(0);
 
         try {
             // Add your data
@@ -63,7 +63,7 @@ public class FetchData extends AsyncTask<String, Integer, String> {
                     JSONArray buildings = new JSONArray(json);
                     DatabaseHelper db = new DatabaseHelper(context);
                     db.updateDatabase(buildings);
-                    return "Db Updated";
+                    return 1;
 
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -81,6 +81,107 @@ public class FetchData extends AsyncTask<String, Integer, String> {
 
         }
 
-        return "Error";
+        publishProgress(1);
+        return 0;
+    }
+
+    protected void onPostExecute(Integer status) {
+        hideProgress();
+        switch (status) {
+            case 0:
+                showWarning("Warning", "Tidak bisa mengakses server");
+                break;
+            case 1:
+                showInfo("Success", "Database berhasil diupdate");
+                break;
+            default:
+                showError("Error", "Terjadi kesalahan");
+                break;
+
+        }
+    }
+
+
+    protected void onProgressUpdate(Integer... progress) {
+        super.onProgressUpdate(progress);
+    }
+
+
+    public void showProgress(String titleText, @Nullable String contentText) {
+        hideProgress();
+        dialogProgress = new SweetAlertDialog(context, SweetAlertDialog.PROGRESS_TYPE);
+        dialogProgress.setTitleText(titleText);
+        dialogProgress.setCancelable(false);
+        if (contentText != null)
+            dialogProgress.setContentText(contentText);
+        dialogProgress.show();
+    }
+
+    public void setProgressIndicator(@Nullable String titleText, @Nullable String contentText) {
+        if (dialogProgress != null && dialogProgress.isShowing()) {
+            if (titleText != null)
+                dialogProgress.setTitle(titleText);
+            if (contentText != null)
+                dialogProgress.setContentText(contentText);
+        }
+    }
+
+    public void hideProgress() {
+        if (dialogProgress != null && dialogProgress.isShowing())
+            dialogProgress.dismiss();
+    }
+
+    public void showWarning(String titleText, @Nullable String contentText) {
+        hideWarning();
+        dialogWarning = new SweetAlertDialog(context, SweetAlertDialog.WARNING_TYPE);
+        dialogWarning.setTitleText(titleText);
+        if (contentText != null)
+            dialogWarning.setContentText(contentText);
+        dialogWarning.show();
+    }
+
+    public void hideWarning() {
+        if (dialogWarning != null && dialogWarning.isShowing())
+            dialogWarning.dismiss();
+    }
+
+    public void showInfo(String titleText, @Nullable String contentText) {
+        hideInfo();
+        dialogInfo = new SweetAlertDialog(context, SweetAlertDialog.NORMAL_TYPE);
+        dialogInfo.setTitleText(titleText);
+        if (contentText != null)
+            dialogInfo.setContentText(contentText);
+        dialogInfo.show();
+    }
+
+    public void showInfo(String titleText, @Nullable String contentText,
+                         @Nullable SweetAlertDialog.OnSweetClickListener listener) {
+        hideInfo();
+        dialogInfo = new SweetAlertDialog(context, SweetAlertDialog.NORMAL_TYPE);
+        dialogInfo.setTitleText(titleText);
+        if (listener != null)
+            dialogInfo.setConfirmClickListener(listener);
+        if (contentText != null)
+            dialogInfo.setContentText(contentText);
+        dialogInfo.show();
+    }
+
+    public void hideInfo() {
+        if (dialogInfo != null && dialogInfo.isShowing())
+            dialogInfo.dismiss();
+    }
+
+    public void showError(String titleText, @Nullable String contentText) {
+        hideError();
+        dialogError = new SweetAlertDialog(context, SweetAlertDialog.ERROR_TYPE);
+        dialogError.setTitleText(titleText);
+        if (contentText != null)
+            dialogError.setContentText(contentText);
+        dialogError.show();
+    }
+
+    public void hideError() {
+        if (dialogError != null && dialogError.isShowing())
+            dialogError.dismiss();
     }
 }
