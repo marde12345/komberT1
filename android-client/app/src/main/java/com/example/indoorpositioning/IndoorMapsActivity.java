f78381a362f13fcb15cd2c806643841cb3cb9d12
marde12345 <32907132+marde12345@users.noreply.github.com>
2019-11-07 00:36:03 +0700

    add lt3 geojson

--- android-client/app/src/main/java/com/example/indoorpositioning/IndoorMapsActivity.java
+++ android-client/app/src/main/java/com/example/indoorpositioning/IndoorMapsActivity.java
@@ -3,24 +3,50 @@ package com.example.indoorpositioning;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
 
+import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.FillLayer;
+import com.mapbox.mapboxsdk.style.layers.LineLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+
+import static com.mapbox.mapboxsdk.style.expressions.Expression.exponential;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.interpolate;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.stop;
+import static com.mapbox.mapboxsdk.style.expressions.Expression.zoom;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.fillOpacity;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineColor;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineOpacity;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineWidth;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IndoorMapsActivity extends BaseActivity {
 
     private MapView mapView;
+    private GeoJsonSource indoorBuildingSource;
+    private List<List<Point>> boundingBoxList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Maps");
 
-        Mapbox.getInstance(this, "pk.eyJ1IjoibWFyZGUxMjM0NSIsImEiOiJjazJseDl4bXcwYzBkM2NwbjV6emlzaHNwIn0.jdZJ0w73lMcwnTN4w11HFw");
+        Mapbox.getInstance(this, getString(R.string.apptoken));
         setContentView(R.layout.activity_indoor_maps);
 
         mapView = findViewById(R.id.mapView);
@@ -32,9 +58,21 @@ public class IndoorMapsActivity extends BaseActivity {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
 
-                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+                        final List<Point> boundingBox = new ArrayList<>();
 
+                        boundingBox.add(Point.fromLngLat(-77.03791, 38.89715));
+                        boundingBox.add(Point.fromLngLat(-77.03791, 38.89811));
+                        boundingBox.add(Point.fromLngLat(-77.03532, 38.89811));
+                        boundingBox.add(Point.fromLngLat(-77.03532, 38.89708));
 
+                        boundingBoxList = new ArrayList<>();
+                        boundingBoxList.add(boundingBox);
+
+                        indoorBuildingSource = new GeoJsonSource("indoor-building", loadJsonFromAsset("lt3.geojson"));
+
+                        style.addSource(indoorBuildingSource);
+
+                        loadBuildingLayer(style);
                     }
                 });
             }
@@ -86,6 +124,48 @@ public class IndoorMapsActivity extends BaseActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    private void loadBuildingLayer(@NonNull Style style) {
+// Method used to load the indoor layer on the map. First the fill layer is drawn and then the
+// line layer is added.
+
+        FillLayer indoorBuildingLayer = new FillLayer("indoor-building-fill", "indoor-building").withProperties(
+                fillColor(Color.parseColor("#eeeeee")),
+// Function.zoom is used here to fade out the indoor layer if zoom level is beyond 16. Only
+// necessary to show the indoor map at high zoom levels.
+                fillOpacity(interpolate(exponential(1f), zoom(),
+                        stop(16f, 0f),
+                        stop(16.5f, 0.5f),
+                        stop(17f, 1f))));
+
+        style.addLayer(indoorBuildingLayer);
+
+        LineLayer indoorBuildingLineLayer = new LineLayer("indoor-building-line", "indoor-building").withProperties(
+                lineColor(Color.parseColor("#50667f")),
+                lineWidth(0.5f),
+                lineOpacity(interpolate(exponential(1f), zoom(),
+                        stop(16f, 0f),
+                        stop(16.5f, 0.5f),
+                        stop(17f, 1f))));
+        style.addLayer(indoorBuildingLineLayer);
+    }
+
+    private String loadJsonFromAsset(String filename) {
+// Using this method to load in GeoJSON files from the assets folder.
+
+        try {
+            InputStream is = getAssets().open(filename);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            return new String(buffer, Charset.forName("UTF-8"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
     }
 
 }
